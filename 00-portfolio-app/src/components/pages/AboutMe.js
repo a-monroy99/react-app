06796bc1a623b3c8179be7e241a18ebeedd2d03b
a.liz <amanda.monroy4@gmail.com>
2023-08-13 18:56:09 -0700

    added about me

--- 00-portfolio-app/src/components/pages/AboutMe.js
+++ 00-portfolio-app/src/components/pages/AboutMe.js
@@ -3,14 +3,10 @@ import React from 'react';
 export default function About() {
   return (
     <div>
-      <h1>About Page</h1>
+      <h2 className='aboutMe'>About Page</h2>
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+        A growing and learning environment is what is most important to me. As of this year (2023). I realized what that a growing mindset is helpful. 
+        Challenging obstacles what once seemed like an unaccomplishable task; I now view as a way to grow and learn.
       </p>
     </div>
   );
